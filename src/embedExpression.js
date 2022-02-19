da8b3ac0eb50871dde9364100090cce877b0f3e0
rillomsholeh <rmsholeh76@gmail.com>
2022-02-19 19:15:05 +0700

    update

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,13 +2,33 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getBatch() {
+    return "Batch30-Dumbways.id";
+  }
 
   //Create a variable here
+  const Nama = "Sholeh";
+  let jurusanKuliah = "Tehnik Informatika";
+
+  // variable aray
+  const namaMahasiswa = ["muhammad sholeh", "ahmad zawawi", "slamet budiono"];
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h3>Muhammad {Nama}</h3>
+      <h1>{jurusanKuliah}</h1>
+      <p>{getBatch()}</p>
+
+      {/* menampilkan variable array namaMahasiswa */}
+      <h4>
+        {namaMahasiswa[0]}
+        <br />
+        {namaMahasiswa[1]}
+      </h4>
+    </div>
   );
 }
 
+// export default embedExpresion
 export default EmbedExpression;
