8eae24e85223d67ff5890b88069a5ee71b55f392
rillomsholeh <rmsholeh76@gmail.com>
2022-04-07 17:12:39 +0700

    update

--- src/props.js
+++ src/props.js
@@ -5,10 +5,7 @@ import List from "./components/list";
 
 function Props() {
   return (
-    // Code Inside div
     <div>
-      {/* <List Name="robby" Email="robby@mail.com" type="number" /> */}
-
       <List carName="BMW" />
       <List carName="Lamborgini" />
       <List carName="Perari" />
